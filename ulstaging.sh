daae5a8afdb4ed9b7ab8ea8d88af61cf1e24f30e
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-26 15:01:53 +0100

    Fix wrong field in ulstaging.

--- ulstaging.sh
+++ ulstaging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "<ul>"
-lsstaging.sh | cut -d " " -f 4,1 | sed 's@^\([^ ]*\) \(.*\)$@<li><a href="\2">\1</a></li>@'
+lsstaging.sh | cut -d " " -f 5,1 | sed 's@^\([^ ]*\) \(.*\)$@<li><a href="\2">\1</a></li>@'
 echo "</ul>"
