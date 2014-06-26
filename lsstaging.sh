8bd69b95624c9e9f384a5fde97cc472b95a65ef1
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-26 12:13:41 +0100

    Add status to lsstaging.

--- lsstaging.sh
+++ lsstaging.sh
@@ -6,13 +6,14 @@ for f in ${STAGING_D}/*
 do
   SL_name_from_file "${f}"    # -> name
   SL_unprefix_name  "${name}" # -> uname
+  SL_url_from_name  "${name}" # -> url
 
   if grep -q "mkstaging" "${f}"
   then
-    SL_port_from_file "${f}" # -> port
-    SL_dir_from_file  "${f}" # -> dir
+    SL_probe_config "${f}"    # -> port, dir
+    SL_test_site_up "${url}" # -> status
 
-    echo "${uname} valid ${port} ${dir}"
+    echo "${uname} valid ${port} ${dir} ${status}"
   else
     echo "${uname} invalid"
   fi
