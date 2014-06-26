84d8cab5d2507af2c7df2f7bab3ed99cc6a5eabc
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-26 13:50:18 +0100

    Add URL to lsstaging.

--- lsstaging.sh
+++ lsstaging.sh
@@ -10,10 +10,10 @@ do
 
   if grep -q "mkstaging" "${f}"
   then
-    SL_probe_config "${f}"    # -> port, dir
+    SL_probe_config "${f}"   # -> port, dir
     SL_test_site_up "${url}" # -> status
 
-    echo "${uname} valid ${port} ${dir} ${status}"
+    echo "${uname} valid ${port} ${dir} ${url} ${status}"
   else
     echo "${uname} invalid"
   fi
