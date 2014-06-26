9c53e7edf741bbc76ddeb5c978ec24d68a6d90d4
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-26 15:04:40 +0100

    Forgot to SNOP off status code.

--- ulstaging.sh
+++ ulstaging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "<ul>"
-lsstaging.sh | cut -d " " -f 1,5,6 | sed 's@^\([^ ]*\) \([^ ]*\) \([a-z]*\)@<li><a href="\2">\1 (currently \3)</a></li>@'
+lsstaging.sh | cut -d " " -f 1,5,6 | sed 's@^\([^ ]*\) \([^ ]*\) \([a-z]*\).*$@<li><a href="\2">\1 (currently \3)</a></li>@'
 echo "</ul>"
